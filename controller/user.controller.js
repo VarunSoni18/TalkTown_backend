3ea3d314978ea0f580ecfd10e7ee9f78510ebb64
Varun soni <varunsoni312004@gmail.com>
2026-09-11 16:59:38 +0530

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -41,6 +41,9 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
+    if (!user) {
+  return res.status(400).json({ error: "Invalid user credential, user didn't exist" });
+}
     const isMatch = await bcrypt.compare(password, user.password);
     if (!user || !isMatch) {
       return res.status(400).json({ error: "Invalid user credential" });
